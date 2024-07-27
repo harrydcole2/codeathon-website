bea72dc3bd69b9c3ecdf7bd4e0cf685f13836086
Harrison Cole <harrydcole2@gmail.com>
2024-07-27 10:02:22 -0600

    more initial changes

---
+++ backend/src/main/java/com/redletterbookclub/red_letter_book_club/RedLetterBookClubApplication.java
@@ -0,0 +1,13 @@
+package com.redletterbookclub.red_letter_book_club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedLetterBookClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedLetterBookClubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/redletterbookclub/red_letter_book_club/RedLetterBookClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redletterbookclub.red_letter_book_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedLetterBookClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
