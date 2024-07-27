9197c40d113bfe30368088028eae95ac8afd3096
Harrison Cole <harrydcole2@gmail.com>
2024-07-27 17:27:46 -0600

    revert app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -9,41 +8,7 @@ import Store from "./pages/Store";
 import About from "./pages/About";
 import NavBar from "./components/NavBar";
 
-import axios from "axios";
-
 function App() {
-  const [username, setUsername] = useState("newuser123");
-  const [password, setPassword] = useState("securepassword123");
-  const [loginResult, setLoginResult] = useState(null);
-
-  async function loginUser(e) {
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/user/login?username=newuser123&password=securepassword123",
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      console.log(response.data);
-      setLoginResult(response.data);
-    } catch (error) {
-      console.error(
-        "Login error:",
-        error.response ? error.response.data : error.message
-      );
-      setLoginResult({
-        error: error.response ? error.response.data : error.message,
-      });
-    }
-  }
-
-  useEffect(() => {
-    loginUser();
-  }, []);
-
   return (
     <ErrorBoundary>
       <NavBar />
