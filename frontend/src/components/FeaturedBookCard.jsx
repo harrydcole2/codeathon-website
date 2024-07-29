84a85cf8081f64897b8afd2dc3a0eb1169c1c16c
Harrison Cole <harrydcole2@gmail.com>
2024-07-29 12:15:23 -0600

    Featured Book Card above

--- frontend/src/components/FeaturedBookCard.jsx
+++ frontend/src/components/FeaturedBookCard.jsx
@@ -30,122 +30,97 @@ const FeaturedBookCard = ({ book }) => {
   return (
     <Card
       sx={{
-        display: "flex",
         width: "100%",
-        minHeight: "400px",
         borderRadius: 2,
         mb: 2,
         boxShadow: 3,
       }}
     >
-      <CardMedia
-        component="img"
-        sx={{ width: 300, objectFit: "cover" }} // TODO: needs to be dynamic and not set the height
-        image={"keep-calm.jpg"}
-        alt={book.title}
-      />
-      <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1, p: 1 }}>
-        <CardContent
-          sx={{
-            flex: "1 0 auto",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                mb: 1,
-              }}
-            >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <Typography variant="h5" component="div" sx={{ mr: 2 }}>
-                  {book.title}
-                </Typography>
-                <Typography variant="h6" color="text.secondary">
-                  {book.author}
-                </Typography>
-              </Box>
-            </Box>
-            <Typography
-              variant="body1"
-              color="text.secondary"
-              sx={{
-                fontSize: "1.1rem",
-                maxHeight: expanded ? "none" : "4.5rem",
-                // overflow: "hidden",
-                // textOverflow: "ellipsis",
-                display: "-webkit-box",
-                WebkitLineClamp: expanded ? "none" : 2,
-                WebkitBoxOrient: "vertical",
-              }}
-            >
-              {book.description}
-            </Typography>
-          </Box>
-          {/* delete these obviously... */}
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          {expanded && (
-            <>
-              <Box sx={{ mt: 4 }}>
-                <Divider sx={{ mb: 1 }} />
-                {book.reviews.map((review) => (
-                  <Review key={review.id} review={review} />
-                ))}
-              </Box>
-            </>
-          )}
-          <Box
-            sx={{ display: "flex", justifyContent: "flex-end", mt: 1, gap: 1 }}
-          >
-            <Button
-              size="small"
-              variant="outlined"
-              sx={{
-                borderColor: "#9a0147",
-                color: "#9a0147",
-                "&:hover": { borderColor: "#9a0147", color: "#9a0147" },
-              }}
-            >
-              Open Discussion
-            </Button>
-            <Button
-              size="small"
-              variant="outlined"
-              sx={{
-                borderColor: "#9a0147",
-                color: "#9a0147",
-                "&:hover": { borderColor: "#9a0147", color: "#9a0147" },
-              }}
-            >
-              Review
-            </Button>
-            <Button
-              onClick={handleExpandClick}
-              size="small"
-              variant="contained"
-              sx={{
-                bgcolor: "#9a0147",
-                color: "white",
-                "&:hover": { bgcolor: "#7a0138" },
-              }}
-            >
-              {expanded ? "Show Less" : "Show More"}
-            </Button>
-          </Box>
-        </CardContent>
+      <Box sx={{ display: "flex", height: "50%", bgcolor: "grey.300" }}>
+        <Box sx={{ flex: 1 }} />
+        <CardMedia
+          component="img"
+          sx={{ width: 200, objectFit: "cover" }} // TODO: needs to be dynamic and not set the height
+          image={"keep-calm.jpg"}
+          alt={book.title}
+        />
+        <Box sx={{ flex: 1 }} />
       </Box>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "50%",
+          padding: "24px",
+        }}
+      >
+        <Box>
+          <Typography variant="h5" component="div" sx={{ mb: 1 }}>
+            {book.title}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" sx={{ mb: 2 }}>
+            {book.author}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            sx={{
+              fontSize: "1.1rem",
+            }}
+          >
+            {book.description}
+          </Typography>
+        </Box>
+        {expanded && (
+          <>
+            <Box sx={{ mt: 4 }}>
+              <Divider sx={{ mb: 1 }} />
+              {book.reviews.map((review) => (
+                <Review key={review.id} review={review} />
+              ))}
+            </Box>
+          </>
+        )}
+        <Box
+          sx={{ display: "flex", justifyContent: "flex-end", mt: 2.5, gap: 1 }}
+        >
+          <Button
+            size="small"
+            variant="outlined"
+            sx={{
+              borderColor: "#9a0147",
+              color: "#9a0147",
+              "&:hover": { borderColor: "#9a0147", color: "#9a0147" },
+            }}
+          >
+            Open Discussion
+          </Button>
+          <Button
+            size="small"
+            variant="outlined"
+            sx={{
+              borderColor: "#9a0147",
+              color: "#9a0147",
+              "&:hover": { borderColor: "#9a0147", color: "#9a0147" },
+            }}
+          >
+            Review
+          </Button>
+          <Button
+            onClick={handleExpandClick}
+            size="small"
+            variant="contained"
+            sx={{
+              bgcolor: "#9a0147",
+              color: "white",
+              "&:hover": { bgcolor: "#7a0138" },
+            }}
+          >
+            {expanded ? "Show Less" : "Show More"}
+          </Button>
+        </Box>
+      </CardContent>
     </Card>
   );
 };
